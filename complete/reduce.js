bff8e60af8eb68987487082a9728875445b5e0d4
Scott DeVito <scottadevito@gmail.com>
2017-09-24 19:02:11 -0400

    Add reduce examples

--- complete/reduce.js
+++ complete/reduce.js
@@ -7,3 +7,18 @@ const peppers = [
   { name: 'carolina reaper', hotness: 9 },
   { name: "dragon's breath", hotness: 10 },
 ];
+
+// Return the sum of the total hotness of the peppers in the pepper array - for loop
+let sumOfAllLoopHotness = 0;
+for (i = 0; i < peppers.length; i++) {
+  sumOfAllLoopHotness = sumOfAllLoopHotness + peppers[i].hotness;
+}
+
+console.log('Sum of all hotness - for loop: ', sumOfAllLoopHotness);
+
+// Return the sum of the total hotness of the peppers in the pepper array - reduce
+let sumOfAllReduceHotness = peppers.reduce((sum, pepper) => {
+  return sum + pepper.hotness;
+}, 0);
+
+console.log('Sum of all hotness - reduce: ', sumOfAllReduceHotness);
--- not-complete/reduce.js
+++ not-complete/reduce.js
@@ -7,3 +7,9 @@ const peppers = [
   { name: 'carolina reaper', hotness: 9 },
   { name: "dragon's breath", hotness: 10 },
 ];
+
+// Return the sum of the total hotness of the peppers in the pepper array - for loop
+//console.log('Sum of all hotness - for loop: ', sumOfAllLoopHotness);
+
+// Return the sum of the total hotness of the peppers in the pepper array - reduce
+//console.log('Sum of all hotness - reduce: ', sumOfAllReduceHotness);
