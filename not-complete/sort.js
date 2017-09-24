8d37dbe4a6d823c5b4a2d66d3fc9b271d3e2ac15
Scott DeVito <scottadevito@gmail.com>
2017-09-24 19:22:01 -0400

    Add sort examples

--- not-complete/sort.js
+++ not-complete/sort.js
@@ -1,3 +1,5 @@
+const simpleArray = ['cayenne', 'habanero', 'bell', 'jalapeno', 'ghost'];
+
 const peppers = [
   { name: 'cayenne', hotness: 3 },
   { name: 'habanero', hotness: 5 },
@@ -7,3 +9,12 @@ const peppers = [
   { name: 'carolina reaper', hotness: 9 },
   { name: "dragon's breath", hotness: 10 },
 ];
+
+// Sort a simple array alphabetically ===================
+//console.log('Simple sort: ', simpleSort);
+
+// Sort peppers on object name ==========================
+//console.log('Alpha sort: ', alphaSortPeppers);
+
+// Sort peppers based on object number ==================
+//console.log('Number sort: ', numSortPeppers);
